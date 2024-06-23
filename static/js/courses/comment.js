bf1c44d6e8e3df67346a1b0f0807a8633d938b1c
Jawine <vaselesstolarov813@gmail.com>
2024-06-23 21:56:35 +0500

    Added the ability to reply to comments and show the comment tree

--- static/js/courses/comment.js
+++ static/js/courses/comment.js
@@ -7,142 +7,167 @@ const Comments = document.querySelector('#Comments')
 const comments_form = document.querySelector('#CreateTaskCommentForm')
 const TaskCommentList = document.querySelector('#TaskCommentList')
 const csrfToken = document.querySelector('[name="csrfmiddlewaretoken"]')
+const commentList = document.querySelector('#TaskCommentList')
+var url = ''
 
-export function renderTaskComments (list, comments) {
-    list.innerHTML = ''
-    if (comments.length > 0) {
-        comments.forEach(comment => {
-            console.log(comment)
-            renderTaskComment(comment, list)
+export function renderTaskComments(list, comments, url) {
+    list.innerHTML = '';
+    url = url
+
+    comments.forEach(comment => {
+        const taskCommentElement = renderTaskComment(comment);
+        list.appendChild(taskCommentElement); // Append each top-level comment to the list
+    });
+}
+
+ const getTaskComments = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    renderTaskComments(commentList, data.data.comments)
+ }
+
+// Recursive function to render a single comment and its children
+const renderTaskComment = (comment) => {
+    const div = document.createElement('div');
+    div.classList.add('comment_item');
+    div.id = `comment-${comment.id}`;
+
+    // Create comment's header
+    const div_header = document.createElement('div');
+    div_header.classList.add('comment_item__header');
+
+    const div_header_left = document.createElement('div');
+    div_header_left.classList.add('comment_item__header__left');
+    div_header_left.innerHTML += `<img src="${comment.user.ava}" alt="${comment.user.username}" />`;
+    div_header_left.innerHTML += `<a href="/users/${comment.user.username}">${comment.user.username}</a>`;
+
+    const div_header_right = document.createElement('img');
+    div_header_right.classList.add('comment_item__header__icon');
+    div_header_right.src = '/static/icons/MenuVertical.svg';
+    div_header_right.alt = 'MenuVertical';
+
+    const div_header_right_menu = document.createElement('div');
+    div_header_right_menu.classList.add('comment_item__header__menu');
+    div_header_right_menu.style.opacity = 0;
+    div_header_right_menu.style.display = 'none';
+
+    const update_comment_button = document.createElement('div');
+    update_comment_button.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> Update`;
+    div_header_right_menu.appendChild(update_comment_button);
+
+    const delete_comment_button = document.createElement('div');
+    delete_comment_button.innerHTML = `<i class="fa-regular fa-trash-can"></i> Delete`;
+    div_header_right_menu.appendChild(delete_comment_button);
+
+    delete_comment_button.addEventListener('click', async () => {
+        await confirmGlobalWindow("Do you want to delete this comment?").then(async (confirmed) => {
+            console.log("User's choice: ", confirmed);
+            if (confirmed) {
+                await fetch(`/api/courses/tasks/${TaskId}/comments/${comment.id}/delete`, {
+                    method: 'DELETE',
+                }).then(() => {
+                    createGlobalMessage("Message deleted successfully!");
+                    div.remove();
+                });
+            }
+        });
+    });
+
+    showHideElement(div_header_right, div_header_right_menu);
+
+    div_header.appendChild(div_header_left);
+    div_header.appendChild(div_header_right);
+    div_header.appendChild(div_header_right_menu);
+    div.appendChild(div_header);
+
+    // Create comment's text
+    const div_text = document.createElement('div');
+    div_text.classList.add('comment_item__text');
+    div_text.innerHTML = comment.message;
+    div.appendChild(div_text);
+
+    // Create comment's footer
+    const div_footer = document.createElement('div');
+    div_footer.classList.add('comment_item__footer');
+
+    const div_footer_left = document.createElement('div');
+    div_footer_left.classList.add('comment_item__footer__left');
+
+    const div_footer_left_smile_button = document.createElement('a');
+    div_footer_left_smile_button.classList.add('comment_item__footer__left__smile__button');
+    div_footer_left_smile_button.innerHTML = `<span class="material-symbols-outlined">favorite</span><i>${comment.likes.count}</i>`;
+    changeLikeStyle(div_footer_left_smile_button, comment.is_liked);
+    div_footer_left_smile_button.addEventListener('click', (e) => {
+        fetch(`/api/courses/tasks/${TaskId}/comments/${comment.id}/react/`, {
+            method: 'POST',
         })
+            .then(response => response.json())
+            .then(data => {
+                let like_count_place =  e.target.parentNode.querySelector('i')
+                let like_count = 0
+
+                if (like_count_place) {
+                    like_count = like_count_place ? parseInt(like_count_place.innerHTML) : null
+                }
+
+                if (data.message === 'Like added successfully!') {
+                    if (like_count_place) {
+                        like_count_place.innerHTML = like_count+1
+                    }
+
+                    changeLikeStyle(div_footer_left_smile_button, true);
+                } else {
+                     if (like_count_place) {
+                        like_count_place.innerHTML = like_count-1
+                    }
+
+                    changeLikeStyle(div_footer_left_smile_button, false);
+                }
+            });
+    });
+
+    const div_footer_left_span = document.createElement('u');
+
+    const div_footer_left_reply_button = document.createElement('a');
+    div_footer_left_reply_button.innerHTML = `Reply`;
+
+    const div_form = document.createElement('div');
+    div_form.classList.add('comment_item__form');
+
+    const div_form_element = createTaskCommentForm(div_form);
+    div_form_element.style.opacity = 0;
+    div_form_element.style.display = 'none';
+
+    showHideElement(div_footer_left_reply_button, div_form_element);
+
+    div_footer_left.appendChild(div_footer_left_smile_button);
+    div_footer_left.appendChild(div_footer_left_span);
+    div_footer_left.appendChild(div_footer_left_reply_button);
+
+    const div_footer_right = document.createElement('span');
+    div_footer_right.innerHTML = comment.created;
+
+    div_footer.appendChild(div_footer_left);
+    div_footer.appendChild(div_footer_right);
+    div.appendChild(div_footer);
+
+    // Recursive rendering of child comments
+    const div_comments = document.createElement('div');
+    div_comments.classList.add('comment_item__comments');
+
+    if (comment.children && comment.children.length > 0) {
+        comment.children.forEach(childComment => {
+            const childCommentElement = renderTaskComment(childComment);
+            div_comments.appendChild(childCommentElement);
+        });
     }
-}
 
-const renderTaskComment = (comment, list) => {
-  const div = document.createElement('div')
-  div.classList.add('comment_item')
+    div.appendChild(div_form);
+    div.appendChild(div_comments);
 
-  // TODO: Create comment's header
-  const div_header = document.createElement('div')
-  div_header.classList.add('comment_item__header')
-
-  const div_header_left = document.createElement('div')
-  div_header_left.classList.add('comment_item__header__left')
-  div_header_left.innerHTML += `<img src="${comment.user.ava}" alt="${comment.user.username}" />`
-  div_header_left.innerHTML += `<a href="/users/${comment.user.username}">${comment.user.username}</a>`
-
-  const div_header_right = document.createElement('img')
-  div_header_right.classList.add('comment_item__header__icon')
-  div_header_right.src = '/static/icons/MenuVertical.svg'
-  div_header_right.alt = 'MenuVertical'
-
-  const div_header_right_menu = document.createElement('div')
-  div_header_right_menu.classList.add('comment_item__header__menu')
-
-  div_header_right_menu.style.opacity = 0
-  div_header_right_menu.style.display = 'none'
-
-  const update_comment_button = document.createElement('div')
-  update_comment_button.innerHTML = `<i class="fa-regular fa-pen-to-square"></i> Update`
-  div_header_right_menu.appendChild(update_comment_button)
-
-  const delete_comment_button = document.createElement('div')
-  delete_comment_button.innerHTML = `<i class="fa-regular fa-trash-can"></i> Delete`
-  div_header_right_menu.appendChild(delete_comment_button)
-
-  delete_comment_button.addEventListener('click', async () => {
-      await confirmGlobalWindow("Do you want to delete this comment?").then(async (confirmed) => {
-          console.log("User's choice: ", confirmed);
-          if (confirmed) {
-              await fetch(`/api/courses/tasks/${TaskId}/comments/${comment.id}/delete`, {
-                  method: 'DELETE',
-              })
-                  .then(() => {
-                      createGlobalMessage("Message deleted successfully!")
-                      list.removeChild(div)
-                  })
-          }
-      });
-  })
-
-  showHideElement(div_header_right, div_header_right_menu)
-
-  div_header.appendChild(div_header_left)
-  div_header.appendChild(div_header_right)
-  div_header.appendChild(div_header_right_menu)
-  div.appendChild(div_header)
-
-  // TODO: Create comment's text
-  const div_text = document.createElement('div')
-  div_text.classList.add('comment_item__text')
-  div_text.innerHTML = comment.message
-  div.appendChild(div_text)
-
-  // TODO: Create comment's footer
-  const div_footer = document.createElement('div')
-  div_footer.classList.add('comment_item__footer')
-
-  const div_footer_left = document.createElement('div')
-  div_footer_left.classList.add('comment_item__footer__left')
-
-  const div_footer_left_smile_button = document.createElement('a')
-  div_footer_left_smile_button.classList.add('material-symbols-outlined', 'comment_item__footer__left__smile__button')
-  div_footer_left_smile_button.innerHTML = `favorite`
-  changeLikeStyle(div_footer_left_smile_button, comment.is_liked)
-  div_footer_left_smile_button.addEventListener('click', () => {
-      fetch(`/api/courses/tasks/${TaskId}/comments/${comment.id}/react/`, {
-          method: 'POST',
-      })
-          .then(response => response.json())
-          .then(data => {
-                  if (data.message == 'Like added successfully!') {
-                        changeLikeStyle(div_footer_left_smile_button, true)
-                  } else {
-                        changeLikeStyle(div_footer_left_smile_button, false)
-                  }
-            })
-  })
-
-  const div_footer_left_smile_menu = document.createElement('div')
-  div_footer_left_smile_menu.classList.add('comment_item__footer__left__smile__menu')
-
-  div_footer_left_smile_menu.style.opacity = 0
-  div_footer_left_smile_menu.style.display = 'none'
-
-  showHideElement(div_footer_left_smile_button, div_footer_left_smile_menu)
-
-  const div_footer_left_span = document.createElement('u')
-
-  const div_footer_left_reply_button = document.createElement('a')
-  div_footer_left_reply_button.innerHTML = `Reply`
-
-  const div_form = document.createElement('div')
-  div_form.classList.add('comment_item__form')
-
-  const div_form_element = createTaskCommentForm(div_form)
-  div_form_element.style.opacity = 0
-  div_form_element.style.display = 'none'
-
-  showHideElement(div_footer_left_reply_button, div_form_element)
-
-  const div_footer_left_reply__form = document.createElement('form')
-  div_footer_left_reply__form.classList.add('comment_item__footer__left__reply__form')
-
-  div_footer_left.appendChild(div_footer_left_smile_button)
-  div_footer_left.appendChild(div_footer_left_smile_menu)
-  div_footer_left.appendChild(div_footer_left_span)
-  div_footer_left.appendChild(div_footer_left_reply_button)
-
-  const div_footer_right = document.createElement('span')
-  div_footer_right.innerHTML += comment.created
-
-  div_footer.appendChild(div_footer_left)
-  div_footer.appendChild(div_footer_right)
-  div.appendChild(div_footer)
-  div.appendChild(div_form)
-
-  list.appendChild(div)
-}
+    return div;
+};
 
 
 const createTaskCommentForm = (place) => {
@@ -168,6 +193,16 @@ const createTaskCommentForm = (place) => {
         createGlobalMessage('Message is too long!')
       } else {
         let formData = new FormData(form)
+
+        console.log('FORM DATA: ', formData)
+        let commentItem = e.target.parentNode.parentNode
+
+        if (commentItem.id) {
+            let commentItem_list = commentItem.id.split('-')
+            if (commentItem_list[0] == 'comment') {
+                formData.append('parent_id', parseInt(commentItem_list[1]))
+            }
+        }
 
         sendData(`/api/courses/tasks/${TaskId}/comments`, formData)
         textarea.value = ''
@@ -199,7 +234,8 @@ const sendData = async (path, data) => {
         .then(d => {
             console.log(d)
             createGlobalMessage("Message created successfully!")
-            renderTaskComment(d.comment,TaskCommentList)
+            // renderTaskComment(d.comment,TaskCommentList)
+            getTaskComments(url)
         })
         .catch(error => {
             console.error('ERROR: \n\n', error)
